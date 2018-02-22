3f5cbf3993f5e1305090f473bd14ccfdf2c1b951
Sergon <Sergon@DESKTOP-4I5OBEC.mshome.net>
2018-02-22 14:36:33 -0600

    This is the Brick Breaker project

---
+++ src/videogame/PowerUp.java
@@ -0,0 +1,27 @@
+package videogame;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Sergon
+ */
+public class PowerUp extends Item
+{
+    
+    public PowerUp(int x, int y, int width, int height)
+    {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void tick()
+    {
+        
+    }
+
+    @Override
+    public void render(Graphics g)
+    {
+    }             
+}
